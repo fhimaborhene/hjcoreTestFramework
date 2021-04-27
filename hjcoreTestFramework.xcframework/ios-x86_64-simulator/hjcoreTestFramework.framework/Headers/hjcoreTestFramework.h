93faf81b98fd56c5cbbe307ec1c0c755cad380db
FHIMA Borhene <borhene.fhima@egis.fr>
2021-04-27 12:06:49 +0200

    add xcframework

---
+++ hjcoreTestFramework.xcframework/ios-x86_64-simulator/hjcoreTestFramework.framework/Headers/hjcoreTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  hjcoreTestFramework.h
+//  hjcoreTestFramework
+//
+//  Created by Borhene Fhima on 13/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for hjcoreTestFramework.
+FOUNDATION_EXPORT double hjcoreTestFrameworkVersionNumber;
+
+//! Project version string for hjcoreTestFramework.
+FOUNDATION_EXPORT const unsigned char hjcoreTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hjcoreTestFramework/PublicHeader.h>
+
+
